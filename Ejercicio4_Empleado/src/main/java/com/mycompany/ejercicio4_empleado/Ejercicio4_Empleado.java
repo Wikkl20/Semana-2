df2992c85a221b466c02de93ee619b4aa166f493
Wikkl20 <t01267f@ms.upla.edu.pe>
2025-09-30 16:38:51 -0500

    Add files via upload

---
+++ Ejercicio4_Empleado/src/main/java/com/mycompany/ejercicio4_empleado/Ejercicio4_Empleado.java
@@ -0,0 +1,14 @@
+
+
+package com.mycompany.ejercicio4_empleado;
+
+import Vista.Principal;
+
+public class Ejercicio4_Empleado {
+
+    public static void main(String[] args) {
+       Principal pantalla = new Principal();
+        pantalla.setVisible(true);
+        pantalla.setLocationRelativeTo(null);
+    }
+}
